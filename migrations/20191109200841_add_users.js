0a474542738f2c93ba1e975ba8bcc6d9aecf395f
Nima Boscarino <nima.boscarino@gmail.com>
2019-11-09 20:12:26 -0800

    users, seeded

---
+++ migrations/20191109200841_add_users.js
@@ -0,0 +1,9 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('users', (table) => {
+        table.increments('id')
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('users')
+};
---
+++ seeds/05_users.js.js
@@ -0,0 +1,18 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        { id: 1 },
+        { id: 2 },
+        { id: 3 },
+        { id: 4 },
+        { id: 5 },
+        { id: 6 },
+        { id: 7 },
+        { id: 8 },
+      ]);
+    });
+};
