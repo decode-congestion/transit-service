5afdfcd7aa4748684f8899d5f19a12b768f31a9e
Nima Boscarino <nima.boscarino@gmail.com>
2019-11-09 22:36:12 -0800

    add loot

---
+++ migrations/20191109222955_add_loot.js
@@ -0,0 +1,23 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('loot', (table) => {
+        table.increments('id')
+        table.integer('type')
+        table.integer('sprite')
+        table.integer('modifier')
+        table.string('name')
+
+        table.integer('vehicle_id')
+        table.integer('user_id')
+
+        table.foreign('vehicle_id')
+            .references('vehicles.id')
+            .onDelete('CASCADE')
+        table.foreign('user_id')
+            .references('users.id')
+            .onDelete('CASCADE')
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('roster_vehicles')
+};
